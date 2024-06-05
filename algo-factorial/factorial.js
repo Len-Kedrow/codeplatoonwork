1b4c2995efb0943d8f310cafb85198ec181da0f1
Len_Kedrow <len.kedrow@gmail.com>
2024-06-05 16:06:16 -0400

    adding clone for files from roman-numerals assignment in code platoon

--- algo-factorial/factorial.js
+++ algo-factorial/factorial.js
@@ -1,9 +1,14 @@
-function factorial (num) {
+const factorial = (num) => {
   if (num === 0) {
     return 1
   } else {
     return num * factorial(num - 1)
   }
 }
+
+console.log(factorial(9))
+console.log(factorial(0))
+console.log(factorial(3))
+
 
 module.exports = factorial
---
+++ lecturedata/example.js
@@ -0,0 +1,6 @@
+i = 0; 
+x = ""
+while(i<0){
+    x+= "#";
+    consloe.log(x);
+    i++; }
