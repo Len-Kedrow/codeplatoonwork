b8a457ab2586508b926a5337fe7b567f8b56244b
Len-Kedrow <Len.Kedrow@gmail.com>
2024-06-21 16:23:39 -0400

    cloning all of todays assignments

--- webpage/index.js
+++ webpage/index.js
@@ -1,8 +1,8 @@
 
-
+let userinput = 0
 let total=0; 
-let guess = 0   
-umberOfGuess = 1let n
+let guess = 0 // this will hold user guess input  
+let numberOfGuess = 1
 let screeninfo = "This is a screen"; //string used for screen output. 
 
 
@@ -16,7 +16,7 @@ for(let i=0; i<document.querySelectorAll(".box").length; i++){
 //Single Function for all buttons but clear
 function buttonPressed(){
     if(this.innerHTML=="C"){
-    total=0; 
+    
     currentdigit=0; 
     previouspush=0;
     maths = "none";
@@ -28,7 +28,17 @@ function buttonPressed(){
         
         total = Math.floor(Math.random() *100) +1
         document.querySelector("#screen p").innerHTML = "This is guess " + numberOfGuess  
-       document.querySelector("#screen2").innerHTML = "Use numkeys on screen to enter guess."
+        document.querySelector("#screen2").innerHTML = "Use numkeys on screen to enter guess."
+        userinput = 0;
+        while (userinput===0){
+            if(this.innerHTML=="="){
+                userinput = 1
+            }
+            else{
+                iscorrect(total, "1234")
+            }
+        }
+        
     }
     else if(this.innerHTML == 1 || this.innerHTML == 2 || this.innerHTML == 3 || this.innerHTML == 4 || this.innerHTML == 5 || this.innerHTML == 6 || this.innerHTML == 7 || this.innerHTML == 8 ||this.innerHTML == 9 || this.innerHTML == 0){
         document.querySelector("#screen p").innerHTML = this.innerHTML;
@@ -38,11 +48,11 @@ function buttonPressed(){
     }
 }
 
-const add=(n1, n2) => n1+n2
+// const add=(total, numnum) => if numnum = total{ } 
 
 
 
-function calculator(n1, n2, operator){
-    return(operator(n1, n2));
+// function calculator(n1, n2, operator){
+//     return(operator(n1, n2));
 
-}
+// }
