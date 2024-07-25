8632f99e61b032cc05e374cdd2cc7bca94c686cb
Len-Kedrow <Len.Kedrow@gmail.com>
2024-07-25 13:05:21 -0400

    django school 2 passing all test

--- django_school/django-school-api-II/school_proj/student_app/models.py
+++ django_school/django-school-api-II/school_proj/student_app/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Student(models.Model):
-    name = models.CharField(max_length=255, null = False, blank = False)
-    student_email = models.EmailField(null = False, blank = False)
-    personal_email = models.EmailField(null = False, blank = False)
-    locker_number = models.IntegerField(default=1, null = False, blank = False)
-    locker_combination = models.CharField(max_length=255, null = False, blank = False)
-    good_student = models.BooleanField()
+    name = models.CharField(max_length=255, null = False, blank = False, unique=False)
+    student_email = models.EmailField(null = False, blank = False, unique=True)
+    personal_email = models.EmailField(null = False, blank = False, unique=True)
+    locker_number = models.IntegerField(default=1, null = False, blank = False, unique=True, db_default=110)
+    locker_combination = models.CharField(max_length=255, null = False, blank = False, unique=False, db_default="12-12-12")
+    good_student = models.BooleanField(unique=False, db_default=True)
